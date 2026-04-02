9d93559c56a73a1c0a340a84f739f668cc204a63
Mohammed Sami Al-Ghoraibi <141200991+MOSAMI0@users.noreply.github.com>
2026-04-02 03:14:03 +0300

    Add files via upload

---
+++ Contact Project/Contacts Project on 3 Tier Architecture - Console App1/Program.cs
@@ -0,0 +1,199 @@
+﻿using ContactsBusinessLayer;
+using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts_Console_Project
+{
+    internal class Program
+    {
+
+        static void testFindContact(int ID)
+
+        {
+            clsContact Contact1 = clsContact.Find(ID);
+
+            if (Contact1 != null)
+            {
+                Console.WriteLine(Contact1.FirstName + " " + Contact1.LastName);
+                Console.WriteLine(Contact1.Email);
+                Console.WriteLine(Contact1.Phone);
+                Console.WriteLine(Contact1.Address);
+                Console.WriteLine(Contact1.DateOfBirth);
+                Console.WriteLine(Contact1.CountryID);
+                Console.WriteLine(Contact1.ImagePath);
+            }
+            else
+            {
+                Console.WriteLine("Contact [" + ID + "] Not found!");
+            }
+        }
+
+        static void testAddNewContact()
+
+
+        {
+            clsContact Contact1 = new clsContact
+            {
+                FirstName = "Fadi",
+                LastName = "Maher",
+                Email = "A@a.com",
+                Phone = "010010",
+                Address = "address1",
+                DateOfBirth = new DateTime(1977, 11, 6, 10, 30, 0),
+                CountryID = 1,
+                ImagePath = ""
+            };
+
+            if (Contact1.Save())
+            {
+
+                Console.WriteLine("Contact Added Successfully with id=" + Contact1.ID);
+            }
+
+        }
+
+        static void testUpdateContact(int ID)
+
+        {
+            clsContact Contact1 = clsContact.Find(ID);
+
+            if (Contact1 != null)
+            {
+                //update whatever info you want
+                Contact1.FirstName = "Fadi2";
+                Contact1.LastName = "Maher2";
+                Contact1.Email = "A2@a.com";
+                Contact1.Phone = "2222";
+                Contact1.Address = "222";
+                Contact1.DateOfBirth = new DateTime(1977, 11, 6, 10, 30, 0);
+                Contact1.CountryID = 1;
+                Contact1.ImagePath = "";
+
+                if (Contact1.Save())
+                {
+
+                    Console.WriteLine("Contact updated Successfully ");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Not found!");
+            }
+        }
+
+        static void testDeleteContact(int ID)
+
+        {
+
+            if (clsContact.DeleteContact(ID))
+
+                Console.WriteLine("Contact Deleted Successfully.");
+            else
+                Console.WriteLine("Faild to delete contact.");
+
+        }
+
+
+        //static void ListContacts()
+        //{
+
+        //    DataTable dataTable = clsContact.ListContacts();
+
+        //    Console.WriteLine("Contacts Data:");
+
+        //    foreach (DataRow row in dataTable.Rows)
+        //    {
+        //        Console.WriteLine($"{row["ContactID"]},  {row["FirstName"]} {row["LastName"]}");
+        //    }
+
+        //}
+
+
+        static void ListContacts()
+        {
+            DataTable dataTable = clsContact.ListContacts();
+
+            // Header color
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            Console.WriteLine(
+                $"{"ID",-4} {"First",-10} {"Last",-10} {"Email",-20} {"Phone",-12} {"Country",-8}"
+            );
+
+            Console.ResetColor();
+
+            Console.WriteLine(new string('-', 70));
+
+            int rowIndex = 0;
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                // Alternate row colors
+                Console.ForegroundColor = (rowIndex % 2 == 0)
+                    ? ConsoleColor.Gray
+                    : ConsoleColor.DarkGray;
+
+                Console.WriteLine(
+                    $"{row["ContactID"],-4} " +
+                    $"{row["FirstName"],-10} " +
+                    $"{row["LastName"],-10} " +
+                    $"{row["Email"],-20} " +
+                    $"{row["Phone"],-12} " +
+                    $"{row["CountryID"],-8}"
+                );
+
+                rowIndex++;
+            }
+
+            Console.ResetColor();
+        }
+
+        static void testIsContactExist(int ID)
+        {
+            if (clsContact.isContactExist(ID))
+            {
+                Console.WriteLine("The contact with ID: [" + ID + "] is found!");
+            }
+            else
+            {
+                Console.WriteLine("The contact with ID: [" + ID + "] is not found :(");
+
+            }
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+
+
+            //testFindContact(17);
+            //testAddNewContact();
+            //testUpdateContact(20);
+            //testDeleteContact(20);
+            //testIsContactExist(1);
+            ListContacts();
+
+
+            Console.WriteLine();
+
+            //TestCountry.testFindCountryByID(1);
+            //TestCountry.testFindCountryByName("Jorden");
+            //TestCountry.testAddNewCountry();
+            //TestCountry.testUpdateCountry(1);
+            //TestCountry.testDeleteCountry(66);
+            //TestCountry.testIsCountryExist(1);
+            //TestCountry.testIsCountryExist("Germany");
+            TestCountry.ListCountries();
+
+            //Console.ReadKey();
+
+        }
+    }
+}
---
+++ Contact Project/Contacts Project on 3 Tier Architecture - Console App1/TestCountry.cs
@@ -0,0 +1,189 @@
+﻿using ContactsBusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts_Console_Project
+{
+    public class TestCountry
+    {
+
+
+        public static void testFindCountryByID(int ID)
+
+        {
+            clsCountry Country1 = clsCountry.Find(ID);
+
+            if (Country1 != null)
+            {
+                Console.WriteLine("Name: " + Country1.CountryName);
+                Console.WriteLine("Code: " + Country1.Code);
+                Console.WriteLine("PhoneCode: " + Country1.PhoneCode);
+
+            }
+
+            else
+            {
+                Console.WriteLine("Country [" + ID + "] Not found!");
+            }
+        }
+
+
+        public static void testFindCountryByName(string CountryName)
+
+        {
+            clsCountry Country1 = clsCountry.Find(CountryName);
+
+            if (Country1 != null)
+            {
+                Console.WriteLine("Country [" + CountryName + "] isFound with ID = " + Country1.ID);
+                Console.WriteLine("Name: " + Country1.CountryName);
+                Console.WriteLine("Code: " + Country1.Code);
+                Console.WriteLine("PhoneCode: " + Country1.PhoneCode);
+            }
+
+            else
+            {
+                Console.WriteLine("Country [" + CountryName + "] Is Not found!");
+            }
+        }
+        public static void testAddNewCountry()
+
+
+        {
+            clsCountry Country1 = new clsCountry();
+
+            Country1.CountryName = "Eygpt";
+            Country1.Code = "222";
+            Country1.PhoneCode = "001";
+
+
+            if (Country1.Save())
+            {
+
+                Console.WriteLine("Country Added Successfully with id=" + Country1.ID);
+            }
+
+        }
+
+        public static void testUpdateCountry(int ID)
+
+        {
+            clsCountry Country1 = clsCountry.Find(ID);
+
+            if (Country1 != null)
+            {
+                //update whatever info you want
+                Country1.CountryName = "Egypt";
+                Country1.Code = "111";
+                Country1.PhoneCode = "555";
+
+
+                if (Country1.Save())
+                {
+
+                    Console.WriteLine("Country updated Successfully ");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Country is you want to update is Not found!");
+            }
+        }
+
+        public static void testDeleteCountry(int ID)
+        {
+            if (clsCountry.Delete(ID))
+                Console.WriteLine("Country Deleted Successfully.");
+            else
+                Console.WriteLine("Failed to delete country.");
+        }
+
+        //public static void ListCountries()
+        //{
+        //    DataTable dt = clsCountries.ListCountries();
+
+        //    Console.WriteLine("Countries Data:");
+
+        //    foreach (DataRow row in dt.Rows)
+        //    {
+        //        Console.WriteLine($"{row["CountryID"]}, {row["CountryName"]}");
+        //    }
+        //}
+
+        public static void ListCountries()
+        {
+            DataTable dataTable = clsCountry.ListCountries();
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            Console.WriteLine(
+                $"{"ID",-6} {"Country Name",-26} {"Code",-10} {"PhoneCode",-12}"
+            );
+
+            Console.ResetColor();
+
+            Console.WriteLine(new string('-', 60));
+
+            int rowIndex = 0;
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                Console.ForegroundColor = (rowIndex % 2 == 0)
+                    ? ConsoleColor.Gray
+                    : ConsoleColor.DarkGray;
+
+              
+                string code = row["Code"] == DBNull.Value || string.IsNullOrWhiteSpace(row["Code"].ToString())
+                    ? "NULL"
+                    : row["Code"].ToString();
+
+                string phoneCode = row["PhoneCode"] == DBNull.Value || string.IsNullOrWhiteSpace(row["PhoneCode"].ToString())
+                    ? "NULL"
+                    : row["PhoneCode"].ToString();
+
+                Console.WriteLine(
+                    $"{row["CountryID"],-6} " +
+                    $"{row["CountryName"],-26} " +
+                    $"{code,-10} " +
+                    $"{phoneCode,-12}"
+                );
+
+                rowIndex++;
+            }
+
+            Console.ResetColor();
+        }
+
+        public static void testIsCountryExist(int ID)
+        {
+            if (clsCountry.isCountryExist(ID))
+            {
+                Console.WriteLine("Country with ID [" + ID + "] exists!");
+            }
+            else
+            {
+                Console.WriteLine("Country with ID [" + ID + "] NOT found!");
+            }
+        }
+
+        public static void testIsCountryExist(string name)
+        {
+            if (clsCountry.isCountryExist(name))
+            {
+                Console.WriteLine("Country [" + name + "] exists!");
+            }
+            else
+            {
+                Console.WriteLine("Country [" + name + "] NOT found!");
+            }
+        }
+
+
+
+    }
+}
